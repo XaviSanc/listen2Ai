29f367b7a463c82fe0d0611af07b00262f9e8c73
DESKTOP-RS374LK\Dell <xaviersancort@gmail.com>
2024-05-05 21:01:11 +0200

    Adding some mockups

--- api/user_REST_controller.py
+++ api/user_REST_controller.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 
+from utils.mockups import fill_users_array
+
 
 
 app = APIRouter(prefix="/users")
 
 
 
+USERS = fill_users_array()
 
 @app.get("/v1/test")
 async def get_user_test():
@@ -13,4 +16,4 @@ async def get_user_test():
     """
     """
 
-    return "hello"
+    return USERS
